c839d656c4a57d86d6e3016c45b19c2fd093ba82
Ljuka <ljubo.maricevic@datadesign.me>
2017-07-28 08:08:10 +0200

    Prvi komit

---
+++ functions/web_scanner.py
@@ -0,0 +1,129 @@
+import os
+from urllib2 import Request, OpenerDirector
+import io
+import whois
+
+# note - you would need to pip install this (or add it with GUI in pycharm
+# (file->settings->project->project interperter->install (a green "+" sign)
+from tld import get_tld
+#class web_scanner:
+
+
+def create_dir(directory):
+    import os
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+def write_file(path, data):
+    f = open(path, 'w')
+    f.write(data)
+    f.close()
+
+def get_domain_name(url):
+    from tld import get_tld
+    domain_name = get_tld(url)
+    return domain_name
+
+# this is the linux version
+def get_ip_address(url):
+    # command = "host" + url
+    # process = os.popen(command)
+    # results = str(process.read())
+    #marker = results.find('has address') + 12
+    import socket
+    results = socket.gethostbyname(url)
+    return "\n \n IP adresa: " + results + "\n \n"
+
+# this is the windows version
+# def get_ip_address(url):
+#     command = "nslookup " + url
+#     process = os.popen(command)
+#     results = str(process.read())
+#     # there's a problem - on some websites the ip will be on the 5th line (index 4) and some it will be on different
+#     # so I just take everything
+#     # marker = results.splitlines()[4]
+#     return results
+
+# this will work for linux automatically.
+# Windows users need to download it from here: https://nmap.org/download.html
+def get_nmap(option, domain):
+    #if not option:
+    #   option = "-F"
+    import os
+    command = "nmap " + domain
+    process = os.popen(command)
+    results = str(process.read())
+    return "\n \n NMAP: \n \n " + results + "\n \n"
+
+def get_robots_txt(url):
+    if url.endswith('/'):
+        path = url
+    else:
+        path = url + '/'
+    import urllib2
+    try:
+        req = urllib2.urlopen(path + 'robots.txt', data=None)
+        data = req
+        return "Robot.txt: \n \n " + data.read() + "\n \n"
+    except urllib2.HTTPError, e:
+        return 'Nije moguce naci robots.txt - HTTPError = ' + str(e.code) + '\n\n'
+
+
+# notice that the command in this function will only work on linux, while in windows you have to download whois
+# https://technet.microsoft.com/en-us/sysinternals/whois.aspx
+# You might have to add the directory path of where you unpack whois to the system variables
+# if so, check:
+# http://superuser.com/questions/284342/what-are-path-and-other-environment-variables-and-how-can-i-set-or-use-them
+def get_whois(url):
+    import os
+    command = "whois " + url
+    process = os.popen(command)
+    results = process.read()
+    return "Detalji korisnika sajta (WhoIs): \n \n " + results + "\n \n"
+
+
+# def gather_info(url):
+#     domain_name = get_domain_name(url)
+#     ip_address = get_ip_address(domain_name)
+#     nmap = get_nmap(domain_name)  # ('-F', ip_address) - this is the linux version
+#     robots_txt = get_robots_txt(url)
+#     whois = get_whois(domain_name)
+#     create_report(url, domain_name, ip_address, nmap, robots_txt, whois)
+
+def create_report(sum_scanner):
+    ROOT_DIR = 'Skenirani sajtovi'
+    project_dir = ROOT_DIR
+    create_dir(project_dir)
+    write_file(project_dir + '/report.txt', sum_scanner)
+    # write_file(project_dir + '/domain_name.txt', domain_name)
+    # write_file(project_dir + '/ipaddress.txt', ip_address)
+    # write_file(project_dir + '/nmap.txt', nmap)
+    # write_file(project_dir + '/robots.txt', robots_txt)
+    # write_file(project_dir + '/whois.txt', whois)
+
+def print_u_rezultat(url):
+    import tkMessageBox
+    if url:
+        #tkMessageBox.showwarning('alert title', 'Bad things happened!')
+        domain_name = get_domain_name(url)
+        nmap = get_nmap("-F", domain_name)
+        robots_txt = get_robots_txt(url)
+        whois = get_whois(domain_name)
+        sum_scanner = domain_name + nmap + robots_txt + whois
+        create_report(sum_scanner)
+        ROOT_DIR = 'Skenirani sajtovi'
+        create_dir(ROOT_DIR)
+        return sum_scanner;
+#gather_info('thenewboston', 'https://www.thenewboston.com/')
+#print_u_rezultat("https://www.github.com/")
+
+
+def ddosAttack(url):
+    import socket, sys, os
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((sys.argv[1], 80))
+    for i in range(10000000):
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((sys.argv[1], 80))
+
+    s.close()
---
+++ main.py
@@ -0,0 +1,118 @@
+#from functions.web_scanner import print_u_rezultat
+import sys
+from numpy.core.defchararray import splitlines
+
+sys.path.insert(0, 'functions/')
+import web_scanner
+import sys
+import time
+from PIL import ImageTk
+import PIL.Image
+import threading
+
+try:
+    # for Python2
+    from Tkinter import *  ## notice capitalized T in Tkinter
+except ImportError:
+    # for Python3
+    from tkinter import *  ## notice lowercase 't' in tkinter here
+
+# class FullScreenApp(object):
+    # def __init__(self, master, **kwargs):
+    #     self.master=master
+    #     pad=3
+    #     self._geom='200x200+0+0'
+    #     master.geometry("{0}x{1}+0+0".format(
+    #         master.winfo_screenwidth()-pad, master.winfo_screenheight()-pad))
+    #     master.bind('<Escape>',self.toggle_geom)
+    # def toggle_geom(self,event):
+    #     geom=self.master.winfo_geometry()
+    #     print(geom,self._geom)
+    #     self.master.geometry(self._geom)
+    #     self._geom=geom
+
+
+def writeInTextBox():
+    sk = skenirano.get()
+    if sk != "":
+        # Domen print
+        domain_name = web_scanner.get_domain_name(sk)
+        rezultat.insert(INSERT, domain_name)
+        rezultat.see(END)
+
+        # IP Adresa
+        ip = web_scanner.get_ip_address(domain_name)
+        rezultat.insert(INSERT, "\n \n " + ip + " \n Please wait... \n")
+        rezultat.see(END)
+
+        # NMAP print
+        nmap = web_scanner.get_nmap("-F", domain_name)
+        nmap_splited = nmap.split("\n")
+        for line in nmap_splited:
+            rezultat.insert(INSERT, line)
+            rezultat.see(END)
+            time.sleep(0.1)
+            rezultat.insert(INSERT, "\n")
+            rezultat.see(END)
+            time.sleep(0.1)
+
+        # ROBOTS print
+        robots_txt = web_scanner.get_robots_txt(sk)
+        robots_splited = robots_txt.split("\n")
+        for line in robots_splited:
+            rezultat.insert(INSERT, line)
+            rezultat.see(END)
+            time.sleep(0.1)
+            rezultat.insert(INSERT, "\n")
+            rezultat.see(END)
+            time.sleep(0.1)
+
+        # WHOIS print
+        whois = web_scanner.get_whois(domain_name)
+        whois_splited = whois.split("\n")
+        for line in whois_splited:
+            rezultat.insert(INSERT, line)
+            rezultat.see(END)
+            time.sleep(0.1)
+            rezultat.insert(INSERT, "\n")
+            rezultat.see(END)
+            time.sleep(0.1)
+
+def threadedInTextBox(self):
+    t1 = threading.Thread(target=writeInTextBox)
+    t1.start()
+
+
+
+root = Tk()
+root.resizable(0,0)
+
+
+image1 = PhotoImage(file="1.gif")
+w = image1.width()
+h = image1.height()
+root.geometry("%dx%d+0+0" % (w, h))
+
+panel1 = Label(root, image=image1)
+panel1.pack(side='top', fill='both', expand='yes')
+
+
+skenirano = StringVar()
+url = Label(root, text="Unesite adresu za testiranje: ")
+url_unos = Entry(root, textvariable=skenirano)
+url.place(relx=.1, rely=.2, anchor="n")
+url_unos.place(relx=.5, rely=.2, anchor="n", width=550)
+url_unos.focus_set()
+
+pocni = Button(root, text="Pocni", bg="#2de810")
+pocni.place(relx=.28, rely=.3, anchor="n", width=150)
+
+
+rezultat = Text(root)
+rezultat.place(relx=.5, rely=.4, anchor="n", width=900, height=250)
+skeniraj = Button(root, text="Skeniraj", bg="#2131fa")
+skeniraj.place(relx=.5, rely=.3, anchor="n", width=150)
+skeniraj.bind("<Button-1>", threadedInTextBox)
+
+
+root.mainloop()
